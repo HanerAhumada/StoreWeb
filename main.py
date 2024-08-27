07cb121a76644781c1fab053a2a3245df8083d8f
ositogominola <hanertovary@gmail.com>
2024-08-27 18:55:22 -0500

    Estructuracion de la pagina

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config.from_prefixed_env('FLASK')
+
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
-    pass
+    app.run(debug=True)
