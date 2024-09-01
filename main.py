04fe09d020f386fdde8a58a9bcd3f4464f5cc25a
ositogominola <hanertovary@gmail.com>
2024-08-31 23:15:34 -0500

    Adelanto en el front de la pagina

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from flask_bootstrap import Bootstrap
 
 load_dotenv()
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 app.config.from_prefixed_env('FLASK')
-
 db = SQLAlchemy(app)
+
+@app.context_processor
+def inject_bootstrap():
+    return dict(bootstrap=bootstrap)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
